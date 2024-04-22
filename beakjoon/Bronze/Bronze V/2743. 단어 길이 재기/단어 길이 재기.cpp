70e78e85c8f1345b066edda470e861e186c40367
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-04-23 08:39:31 +0900

    Create 단어 길이 재기.cpp

---
+++ beakjoon/Bronze/Bronze V/2743. 단어 길이 재기/단어 길이 재기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm> // string.length() 메소드를 사용할 수 있게 해줌.
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    cout << str.length();
+    return 0;
+}
