4cd4f07e346e77ce8c6cc77b0361d64e5b32d96d
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-04-23 09:03:27 +0900

    Create Test.cpp

---
+++ basic/07. 연산자/07-3. 할당 연산자/Test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 10;
+    cout << "x : " << x << "\n";
+
+    x += 5;
+    cout << "x : " << x;
+    return 0;
+}
+
+/*
+ * x : 10
+ * x : 15
+ */
