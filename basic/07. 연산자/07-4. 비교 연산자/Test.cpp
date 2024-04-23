12ea8f0851494f5d7ee2b5cbfffd2290627dbeb5
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-04-23 09:04:44 +0900

    Create Test.cpp

---
+++ basic/07. 연산자/07-4. 비교 연산자/Test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int x = 5;
+    int y = 3;
+
+    cout << "0 = False, 1 = True\n";
+    cout << (x == y) << "\n";
+    cout << (x != y) << "\n";
+    cout << (x > y) << "\n";
+    cout << (x >= y) << "\n";
+    cout << (x < y) << "\n";
+    cout << (x <= y) << "\n";
+    return 0;
+}
+
+/*
+ * 0 = False, 1 = True
+ * 0
+ * 1
+ * 1
+ * 1
+ * 0
+ * 0
+ */
