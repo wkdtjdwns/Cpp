44858e14eb4ae06ccefb96f40a206d2244c00df8
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-04-23 09:00:50 +0900

    Create Test.cpp

---
+++ basic/07. 연산자/07-1. 산술 연산자/Test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 10;
+    int b = 2;
+
+    cout << "a + b : " << (a + b) << "\n"; // a + b : 12
+    cout << "a - b : " << (a - b) << "\n"; // a - b : 8
+    cout << "a * b : " << (a * b) << "\n"; // a * b : 20
+    cout << "a / b : " << (a / b) << "\n"; // a / b : 5
+    cout << "a % b : " << (a % b) << "\n"; // a % b : 0
+    return 0;
+}
