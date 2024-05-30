64d12ad3acf29c510d43a2511bdbf025e3f22f99
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-05-30 09:03:14 +0900

    Create 연세대학교.cpp

---
+++ beakjoon/Bronze/Bronze V/15680. 연세대학교/연세대학교.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	string str = (n == 0) ? "YONSEI" : "Leading the Way to the Future";
+
+	cout << str;
+	return 0;
+}
