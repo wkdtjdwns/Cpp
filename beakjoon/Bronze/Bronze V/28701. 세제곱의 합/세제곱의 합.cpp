1aa5d9414ec8baa2dd930ce296735e877ee762c5
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-06 11:50:02 +0900

    Create 세제곱의 합.cpp

---
+++ beakjoon/Bronze/Bronze V/28701. 세제곱의 합/세제곱의 합.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int result = n * (n + 1) / 2;
+
+	cout << result << "\n";
+	cout << result * result << "\n";
+	cout << result * result;
+
+	return 0;
+}
