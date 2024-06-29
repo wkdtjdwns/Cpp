20eefabdf30f33beeb6e59e4c31fea18e20f6e3b
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-06-29 18:11:27 +0900

    [2024-06-29]

---
+++ beakjoon/Bronze/Bronze III/1598. 꼬리를 무는 숫자 나열/꼬리를 무는 숫자 나열.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+	int x, y, xw, yw, xh, yh; cin >> x >> y;
+
+	xw = (x - 1) / 4;
+	yw = (y - 1) / 4;
+
+	xh = (x - 1) % 4;
+	yh = (y - 1) % 4;
+
+	cout << abs(xw - yw) + abs(xh - yh);
+
+	return 0;
+}
