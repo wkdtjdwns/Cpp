951d5009d0fd05e3425f8b770d4f1bd62a341703
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-10 14:12:45 +0900

    [2024-07-10]

---
+++ beakjoon/Bronze/Bronze III/3053. 택시 기하학/택시 기하학.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#define PI 3.1415926535897932
+using namespace std;
+
+int main() {
+
+	double r; cin >> r;
+
+	cout << fixed;
+	cout.precision(6);
+	cout << r * r * PI << "\n" << 2 * r * r << "\n";
+
+    return 0;
+}
