e652e9dfe3c524007825d872ffaad119b64011f2
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-11 22:25:48 +0900

    [2024-07-11]

---
+++ beakjoon/Bronze/Bronze III/4458. 첫 글자를 대문자로/첫 글자를 대문자로.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+    string k;
+    int n; cin >> n;
+
+    cin.ignore();
+    while (n--)
+    {
+        getline(cin, k);
+
+        if ('a' <= k[0] && k[0] <= 'z') {
+            k[0] -= 32;
+        }
+
+        cout << k << '\n';
+    }
+
+    return 0;
+}
