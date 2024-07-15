e9eed32f2ab9442b6bde0c9e3120b679577cf12c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-15 21:08:44 +0900

    [2024-07-15]

---
+++ beakjoon/Bronze/Bronze II/10834. 벨트/벨트.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#define ll long long
+using namespace std;
+
+int main() {
+
+	int m, a = 1, b = 1, s, rNum = 1; cin >> m;
+	bool r = true;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> s;
+		rNum = (rNum / a) * b;
+		r = s == 1 ? !r : r;
+	}
+
+	if (r) { cout << "0 " << rNum; }
+	else { cout << "1 " << rNum; }
+
+
+    return 0;
+}
