f0e1b43c3ada43c9e0dbe81c07d6711b79fa3e21
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-22 19:19:12 +0900

    [2024-07-22]

---
+++ beakjoon/Bronze/Bronze V/21598. SciComLove/SciComLove.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	int n; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "SciComLove\n";
+	}
+
+	return 0;
+}
