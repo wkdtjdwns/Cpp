b6686bd5b4a9297def0ed6b46f1f42071a71993b
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-25 17:24:40 +0900

    [2024-07-25]

---
+++ beakjoon/Bronze/Bronze III/10872. 팩토리얼/팩토리얼.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+
+int factorial(int n) {
+    if (n <= 1) { return 1; }
+    return n * factorial(n - 1);
+}
+
+int main() {
+	fastIo;
+
+    int n; cin >> n;
+    cout << factorial(n);
+
+	return 0;
+}
