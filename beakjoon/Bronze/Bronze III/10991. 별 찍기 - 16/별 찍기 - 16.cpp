7b72e9b9a7ceca522683032b6173147dbf9b502e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-26 20:34:21 +0900

    [2024-07-26]

---
+++ beakjoon/Bronze/Bronze III/10991. 별 찍기 - 16/별 찍기 - 16.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	int n; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - 1 - i; j++) {
+			cout << " ";
+		}
+
+		for (int k = 0; k <= i; k++) {
+			cout << "* ";
+		}
+
+		cout << "\n";
+	}
+
+	return 0;
+}
