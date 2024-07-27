7a419bf610e32738d61502b096311be74481a35b
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-27 16:07:03 +0900

    [2024-07-27]

---
+++ beakjoon/Bronze/Bronze III/11023. 더하기 3/더하기 3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	int n, sum = 0;
+	while (cin >> n)
+	{
+		sum += n;
+	}
+
+	cout << sum;
+
+	return 0;
+}
