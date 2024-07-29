c92d5b0e41f594e2185183e2e4d6a72e7973c500
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-29 16:41:42 +0900

    [2024-07-28]

---
+++ beakjoon/Bronze/Bronze III/17496. 스타후르츠/스타후르츠.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n, t, c, p; cin >> n >> t >> c >> p;
+    cout << (n - 1) / t * c * p;
+
+    return 0;
+}
