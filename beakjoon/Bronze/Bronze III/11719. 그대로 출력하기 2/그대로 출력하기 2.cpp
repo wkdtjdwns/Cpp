5b5994685d63803b90c06486403281fbed6a85a4
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-30 16:08:55 +0900

    [2024-07-30]

---
+++ beakjoon/Bronze/Bronze III/11719. 그대로 출력하기 2/그대로 출력하기 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	string str;
+	while (getline(cin, str))
+	{
+		cout << str << "\n";
+	}
+
+	return 0;
+}
