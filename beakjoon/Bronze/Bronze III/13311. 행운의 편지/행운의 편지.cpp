07ddaf23377f809a4147d5a53004d4ed942e6205
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-07-31 18:13:11 +0900

    [2024-07-31]

---
+++ beakjoon/Bronze/Bronze III/13311. 행운의 편지/행운의 편지.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    cout << -1;
+
+    return 0;
+}
