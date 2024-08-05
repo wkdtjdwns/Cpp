b32fb6d5351fd5666ed7685498db95a4bcd1d2b0
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-05 10:10:46 +0900

    [2024-08-05]

---
+++ beakjoon/Bronze/Bronze IV/31746. SciComLove (2024)/SciComLove (2024).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n; cin >> n;
+    if (n % 2 == 0) {
+        cout << "SciComLove";
+    }
+
+    else {
+        cout << "evoLmoCicS";
+    }
+
+    return 0;
+}
