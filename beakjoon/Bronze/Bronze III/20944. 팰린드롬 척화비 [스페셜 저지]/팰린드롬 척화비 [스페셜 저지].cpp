182df3a07097264d1f0124be617bbec01d942769
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-10 15:25:57 +0900

    [2024-08-10]

---
+++ beakjoon/Bronze/Bronze III/20944. 팰린드롬 척화비 [스페셜 저지]/팰린드롬 척화비 [스페셜 저지].cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	int n; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "s";
+	}
+	
+	return 0;
+}
