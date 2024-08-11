557dbecd9ee64c8819c7ec03544be8ae4467035c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-11 13:32:01 +0900

    [2024-08-11]

---
+++ beakjoon/Bronze/Bronze III/21553. 암호 만들기/암호 만들기.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	string a, p; cin >> a >> p;
+	cout << p;
+
+	return 0;
+}
