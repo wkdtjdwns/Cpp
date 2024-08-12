cc5e49728a276ccc3657aaea992a808aabee9148
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-12 11:40:34 +0900

    [2024-08-12]

---
+++ beakjoon/Bronze/Bronze IV/32025. 체육은 수학과목 입니다/체육은 수학과목 입니다.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int h, w; cin >> h >> w;
+    cout << min(h, w) * 50;
+
+    return 0;
+}
