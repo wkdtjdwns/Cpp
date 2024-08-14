9eb9698635d00aca026d5387100d1c762856f88c
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-14 12:35:33 +0900

    [2024-08-14]

---
+++ beakjoon/Bronze/Bronze III/23805. 골뱅이 찍기 - 돌아간 ㄹ/골뱅이 찍기 - 돌아간 ㄹ.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n; cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n * 3; j++)
+        {
+            cout << "@";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n * 3; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < n * 3; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
