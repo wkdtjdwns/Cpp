38ed28a09a9a0b4c184d7304783b350d3e2f642e
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-14 12:54:49 +0900

    [2024-08-14]

---
+++ beakjoon/Bronze/Bronze III/23810. 골뱅이 찍기 - 뒤집힌 ㅋ/골뱅이 찍기 - 뒤집힌 ㅋ.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n; cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n * 5; j++)
+        {
+            cout << "@";
+        }
+        
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n * 5; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "@";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
