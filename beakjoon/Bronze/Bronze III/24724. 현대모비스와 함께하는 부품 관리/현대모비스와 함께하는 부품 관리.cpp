a4170728d4dd3e596d93e3695d88b0385e502311
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-16 18:58:34 +0900

    [2024-08-16]

---
+++ beakjoon/Bronze/Bronze III/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+	fastIo;
+
+	int t, n, l1, l2, a, b; cin >> t;
+	for (int i = 1; i <= t; i++) {
+		cin >> n >> l1 >> l2;
+		for (int j = 0; j < n; j++) {
+			cin >> a >> b;
+		}
+
+		cout << "Material Management " << i << '\n';
+		cout << "Classification ---- End!\n";
+	}
+
+	return 0;
+}
