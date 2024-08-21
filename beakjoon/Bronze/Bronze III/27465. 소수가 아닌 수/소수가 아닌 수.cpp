8b0d3ee1085227f65de4dba61abe3cf26a4878ef
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-21 19:02:50 +0900

    [2024-08-21]

---
+++ beakjoon/Bronze/Bronze III/27465. 소수가 아닌 수/소수가 아닌 수.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+bool alphabet[26];
+
+int main() {
+	fastIo;
+
+	cout << 1000000000;
+
+	return 0;
+}
