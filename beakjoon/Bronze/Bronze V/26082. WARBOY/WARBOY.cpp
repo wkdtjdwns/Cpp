f106d25abeeb0bb21d873c64ae5f50a81dfcd802
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-23 23:05:40 +0900

    [2024-08-23]

---
+++ beakjoon/Bronze/Bronze V/26082. WARBOY/WARBOY.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int a, b, c; cin >> a >> b >> c;
+    cout << 3 * (b / a) * c;
+
+    return 0;
+}
