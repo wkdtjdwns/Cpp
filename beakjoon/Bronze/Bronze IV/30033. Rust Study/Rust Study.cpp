f9f6922619852b6f23f3fd123702606adb3034f6
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-08-29 21:43:11 +0900

    [2024-08-29]

---
+++ beakjoon/Bronze/Bronze IV/30033. Rust Study/Rust Study.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n; cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    int result = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] <= b[i]) {
+            result++;
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
