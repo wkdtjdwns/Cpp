c44f2037bd491679422e8d0be58f6436d5172e46
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-12 13:14:35 +0900

    [2024-09-12]

---
+++ beakjoon/Bronze/Bronze III/31831. 과민성 대장 증후군/과민성 대장 증후군.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n, m; cin >> n >> m;
+    int total = 0, cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int num; cin >> num;
+        total += num;
+        if (total < 0) {
+            total = 0;
+        }
+
+        if (total >= m) {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
