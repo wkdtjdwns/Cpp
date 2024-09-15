0a1eb7f02e9b260f00f0bcf3545c68a2a60cfaf3
rktrjtrit <128266768+wkdtjdwns@users.noreply.github.com>
2024-09-15 20:26:09 +0900

    [2024-09-15]

---
+++ beakjoon/Bronze/Bronze III/31868. 수박 게임/수박 게임.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#define ll long long
+#define fastIo ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define fixPrint(n) cout << fixed; cout.precision(n);
+using namespace std;
+
+int main() {
+    fastIo;
+
+    int n, k; cin >> n >> k;
+    for (int i = 0; i < n - 1; i++)
+    {
+        k /= 2;
+    }
+
+    cout << k;
+
+    return 0;
+}
